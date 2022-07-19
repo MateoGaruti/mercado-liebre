0aa66559f771b04dc6dad1619d4662f5366888eb
MateoGaruti <mateo.gap@gmail.com>
2022-07-19 15:51:39 -0300

    mejora mercado

--- mercado.js
+++ mercado.js
@@ -3,13 +3,15 @@ let express = require ("express")
 let app = express ();
 
 let path = require ("path");
-
-app.listen (1000, () => console.log ("MERCADO ON")); // INICIO DE LA PAGINA WEB
-
+const { env } = require("process");
 
 const public = path.resolve (__dirname, "./public") // parado en la carpeta public // con esto logro que mi imagen aprezaca
 
 const static = express.static (public) // aca ejecuto la carpeta public 
+
+const port = process.env.PORT || 1000;
+
+app.listen (port, () => console.log ("MERCADO ON")); // INICIO DE LA PAGINA WEB
 
 app.use (static)
 
@@ -17,6 +19,12 @@ app.get ("/",(req, res) => {
     let htmlPath = path.resolve (__dirname, "./views/indes.html")
     res.sendFile (htmlPath)
 })
+
+app.get ("/registro",(req,res)=>{
+    let htmlPath = path.resolve (__dirname, "./views/registro.html")
+    res.sendFile (htmlPath)
+})
+
 
 app.get ("/login",(req,res)=>{
     let htmlPath = path.resolve (__dirname, "./views/login.html")
